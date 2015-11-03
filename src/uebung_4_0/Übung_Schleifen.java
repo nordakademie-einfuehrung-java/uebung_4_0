f6b9d51162ee4ee7df8dcd7a395686f64a2fcb69
14126 <14126@A001-16.nordakademie.intern>
2015-11-03 10:47:57 +0100

    Übung 4.0

---
+++ src/uebung_4_0/Übung_Schleifen.java
@@ -0,0 +1,19 @@
+package uebung_4_0;
+
+public class Übung_Schleifen {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int maximum = 1000000;
+		int summe = 0;
+		int n = 0;
+
+		while ( summe <= maximum) {
+			System.out.println("Zahl " + n + " " + summe);
+			n = n + 1;
+			summe = summe + 1 * n;
+		}
+	}
+
+}
